323e914678bb9753497f527714056fbe77857594
Павел Шамсимухаметов <p.shamsimuhametov@axelot.ru>
2024-05-09 12:53:47 +0500

    Попытка исправить доступ к .env

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
-import { PUBLIC_API_URL } from '$env/static/public'
+import { env } from '$env/dynamic/public'
 
-export const API_URL = PUBLIC_API_URL
+export const API_URL = env.PUBLIC_API_URL
 
 export const FURNACE_FIELDS =
 [
